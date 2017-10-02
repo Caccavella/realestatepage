c0b2196732063a39265ce81ebdcad1c949407df0
Anthony <anthony.caccavella@gmail.com>
2017-10-02 15:14:45 -0600

    Final Commitment!

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -38,18 +38,9 @@ export default class Testimonials extends Component {
                 </div>
                 <div className="testimonialdisplay">
                     <h2>What clients are saying: </h2>
-                    {/* <p >"Lisa helped us so much with selling our home! We were able to get it
-                        under contract within 12 hours from listing, for more than the list price!"
-                    </p>
-                    <p className="individualtestimonial">-Anthony and Chelsea</p> */}
                     <div>
                     {newData}
                     </div>
-                    {/* <p>"Hector isaac mouse yak nelson apple mickey elephant ice savior
-                    Hector isaac mouse yak nelson apple mickey elephant ice savior
-                    Hector isaac mouse yak nelson apple mickey elephant ice savior" */}
-                    {/* </p> */}
-                    {/* <p className='individualtestimonial'>-Shika Shika Slim Shady</p> */}
                 </div>
                 <div>
                     <Footer />
